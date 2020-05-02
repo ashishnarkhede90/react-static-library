0d8f34570b28f69fa5336263c1df7ff3a81ebb21
ASHISH NARKHEDE <ashishnarkhede@ASHISHs-MacBook-Air.local>
2020-05-02 15:38:49 -0700

    Fetching data for quotes

--- src/index.js
+++ src/index.js
@@ -48,6 +48,12 @@ const NotHiring = () => {
   )
 }
 
+// Makes API call to fetch quotes
+async function getQuotes() {
+  const result = await fetch('http://philosophy-quotes-api.glitch.me/quotes/');
+  return result;
+}
+
 // Library class component
 class Library extends Component {
 
@@ -56,6 +62,16 @@ class Library extends Component {
     isHiring: true,
     freeBookmarkToday: false
   };
+
+  componentDidMount() {
+    this.setState({quote: 'Getting a quote...'});
+    getQuotes()
+      .then(response => response.json())
+      .then(data =>  {
+        let q = Math.floor(Math.random() * data.length) + 1;
+        return this.setState({quote: data[q].quote + ' - ' + data[q].source})
+      })
+  }
 
   // on click handler (callback)
   toggleLibrary = () => {
@@ -66,11 +82,18 @@ class Library extends Component {
   render() {
     return (
       <div>
+        <div style={{background: 'Orange', padding: '0.3rem', marginBottom: '10px'}}>
+          { this.state.quote }
+        </div> 
+
         { this.state.isHiring ? <Hiring /> : <NotHiring /> }
+        
         <h4 style={{color: 'white', padding: '0.5rem', background: this.state.isOpen ? 'green': 'red'}}>
           The library {this.state.isOpen ? ' is open.' : ' is not open.'} 
         </h4>
+        
         <button onClick={this.toggleLibrary}>{this.state.isOpen ? 'Close' : 'Open'}</button>
+        
         {/* Iterate over the data */}
         <section>
           { this.props.books.map(book => (<Book book={book} key={book.title} freeBookmarkToday={this.state.freeBookmarkToday} />)) }
