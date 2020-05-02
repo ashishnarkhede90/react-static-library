cd006eecdef86f835a220d3520d97f06bf263e66
ASHISH NARKHEDE <ashishnarkhede@ASHISHs-MacBook-Air.local>
2020-05-02 14:39:58 -0700

    Converted Library to class component and added state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 const BookList = [
@@ -32,16 +32,42 @@ const Book = (props) => {
 }
 
 // Library function component
-const Library = (props) => {
+/*const Library = (props) => {
   console.log(props);
   return (
     <div>
-      {/* Iterate over the data */}
+      {/ Iterate over the data /}
       <section>
         { props.books.map(book => (<Book book={book} key={book.title} />)) }
       </section>
     </div>
   )
+}
+*/
+
+// Library class component
+class Library extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: true }
+  }
+
+  render() {
+    console.log('Library', 'class component');
+
+    return (
+      <div>
+        <h4 style={{color: 'white', padding: '0.5rem', background: this.state.isOpen ? 'green': 'red'}}>
+          The library {this.state.isOpen ? ' is open.' : ' is not open.'} 
+        </h4>
+        {/* Iterate over the data */}
+        <section>
+          { this.props.books.map(book => (<Book book={book} key={book.title} />)) }
+        </section>
+      </div>
+    )
+  }
 }
 
 const App = () => {
