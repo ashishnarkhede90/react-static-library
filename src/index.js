67b6057f992234d4c38f502d22709fd4d10d1066
ASHISH NARKHEDE <ashishnarkhede@ASHISHs-MacBook-Air.local>
2020-05-02 14:15:55 -0700

    Iterate over static data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const BookList = [
+  {
+    title: 'A Game of Thrones',
+    author: 'George R. R. Martin',
+    pages: 694
+  },
+  {
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    pages: 600
+  },
+  {
+    title: 'The Sworn Sword',
+    author: 'George R. R. Martin',
+    pages: 800
+  }
+];
+
 const Library = () => (
   <div>
+    {/* Iterate over the data */}
     <section>
-      <p><b>Name: The Alchemist</b></p>
-      <p>by - Paulo Coelho</p>
-      <p>Pages: 250</p>
+      { BookList.map(book => (
+          <section>
+            <p><b>Title: {book.title}</b></p>
+            <p>by - {book.author}</p>
+            <p>Pages: {book.pages}</p>
+          </section>
+        ))
+      }
     </section>
   </div>
 )
