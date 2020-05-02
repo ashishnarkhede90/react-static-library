630113c21fc50e64b1f25687a1ae0ca6f9ebc13f
ASHISH NARKHEDE <ashishnarkhede@ASHISHs-MacBook-Air.local>
2020-05-02 11:59:56 -0700

    Initializing basic Library app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Library() {
+  return (
+    <div>
+      <section>
+        <p><b>Name: The Alchemist</b></p>
+        <p>by - Paulo Coelho</p>
+        <p>Pages: 250</p>
+      </section>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Library />
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
